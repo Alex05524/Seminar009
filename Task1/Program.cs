883adc4a72e8017c0da714d826023a6a058c989b
Alexey <pwz-and_10@mail.ru>
2022-09-04 17:03:05 +0600

    create task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("M: ");
+        int M = Convert.ToInt16(Console.ReadLine());
+        Console.Write("N: ");
+        int N = Convert.ToInt16(Console.ReadLine());
+        if (N > M)
+            for (int i = M; i <= N; i++)
+            {
+                Console.Write($"{i}, ");
+            }
+        else
+            for (int i = N; i <= M; i++);
+    }
+}
