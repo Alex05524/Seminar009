71fb7958e7a717fb37aed876a1a41bb4557e2a15
Alexey <pwz-and_10@mail.ru>
2022-09-04 17:14:07 +0600

    create task3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿int m = InputNumbers("m: ");
+int n = InputNumbers("n: ");
+int A = AkkF(m, n);
+Console.Write($"A(m, n) = {A}");
+
+int AkkF(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (n == 0) return AkkF(m - 1, 1);
+  else return AkkF(m - 1, AkkF(m, n - 1));
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
