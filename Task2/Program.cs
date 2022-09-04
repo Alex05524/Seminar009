0ed533ea5f9dceb89067018918a222d8df2ab130
Alexey <pwz-and_10@mail.ru>
2022-09-04 17:08:25 +0600

    create task2

---
+++ Task2/Program.cs
@@ -0,0 +1,34 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        int M = InpNum("M: ");
+        int N = InpNum("N: ");
+        int temp = M;
+
+        if (M > N)
+        {
+            M = N;
+            N = temp;
+        }
+        PrintSum(M, N, temp = 0);
+
+        void PrintSum(int M, int N, int sum)
+        {
+            sum = sum + N;
+            if (N <= M)
+            {
+                Console.Write($"{sum}");
+                return;
+            }
+            PrintSum(M, N - 1, sum);
+        }
+
+        int InpNum(string input)
+        {
+            Console.Write(input);
+            int output = Convert.ToInt32(Console.ReadLine());
+            return output;
+        }
+    }
+}
